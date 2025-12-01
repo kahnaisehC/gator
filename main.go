3ce5d7e52a54424d554e9269195429acce9d2684
Ian Gabriel Hakanson <gabianhak@gmail.com>
2025-12-01 20:11:14 -0300

    feat: add handlerLogin function

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/kahnaisehC/blog_aggregator/internal/config"
 )
+
+type state struct {
+	Cfg *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return errors.New("the login command expects a username as an arguments")
+	}
+	username := cmd.arguments[0]
+
+	if err := s.Cfg.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Println("The username has been changed successfully!")
+
+	return nil
+}
 
 func main() {
 	cfg, err := config.Read()

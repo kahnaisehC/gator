0e815cf4543e70534a1deb97b66e7a9cc7649b17
Ian Gabriel Hakanson <gabianhak@gmail.com>
2025-12-02 00:22:46 -0300

    feat: create commands and state structs

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/kahnaisehC/blog_aggregator/internal/config"
 )
@@ -14,6 +16,23 @@ type state struct {
 type command struct {
 	name      string
 	arguments []string
+}
+
+type commands struct {
+	cmdMap map[string]func(s *state, cmd command) error
+}
+
+func (cmds *commands) run(s *state, cmd command) error {
+	f, ok := cmds.cmdMap[cmd.name]
+	if !ok {
+		return errors.New(cmd.name + " is not a registered command")
+	}
+
+	return f(s, cmd)
+}
+
+func (cmds *commands) register(name string, f func(*state, command) error) {
+	cmds.cmdMap[name] = f
 }
 
 func handlerLogin(s *state, cmd command) error {
@@ -36,10 +55,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = cfg.SetUser("ian")
-	if err != nil {
-		panic(err)
-	}
+	gatorState := state{}
+	gatorState.Cfg = &cfg
 
-	fmt.Printf("%v\n", cfg)
+	cmds := commands{}
+	cmds.cmdMap = make(map[string]func(s *state, cmd command) error)
+	cmds.register("login", handlerLogin)
+
+	args := os.Args
+	if len(args) < 2 {
+		log.Println("not enough arguments")
+		os.Exit(1)
+	}
+	cmd := command{}
+	cmd.name = args[1]
+	cmd.arguments = args[2:]
+
+	if err = cmds.run(&gatorState, cmd); err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
 }

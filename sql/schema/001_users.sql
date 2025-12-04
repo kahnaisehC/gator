83e49d7dd531a8eda4f7c87edf9f6a5882c63aee
Ian Gabriel Hakanson <gabianhak@gmail.com>
2025-12-04 10:20:10 -0300

    feat: i forgot to push xd

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,5 +1,10 @@
 -- +goose Up
-CREATE TABLE users (id UUID PRIMARY KEY, created_at TIMESTAMP NOT NULL, updated_at TIMESTAMP NOT NULL, name TEXT NOT NULL);
+CREATE TABLE users (
+	id UUID PRIMARY KEY, 
+	created_at TIMESTAMP NOT NULL, 
+	updated_at TIMESTAMP NOT NULL, 
+	name TEXT UNIQUE NOT NULL
+);
 
 -- +goose Down
 DROP TABLE users;
